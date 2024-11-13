b4e63bf07635f4fda76a08065f43c8fb0f974b32
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-13 20:52:36 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -11,28 +11,6 @@ if (rex::isBackend() && rex::getUser()) {
     
     // Add custom data for JavaScript
     rex_view::setJsProperty('medianeo', [
-        'csrf_token' => rex_csrf_token::factory('medianeo')->getValue(),
-        'ajax_url' => rex_url::backendController(['page' => 'medianeo/ajax']), // Dies ist die korrekte AJAX-URL
-        'media_url' => rex_url::backendController(['page' => 'mediapool/media'])
+        'csrf_token' => rex_csrf_token::factory('medianeo')->getValue()
     ]);
 }
-
-// Register Extension Point für die Page
-rex_extension::register('PAGES_PREPARED', function (rex_extension_point $ep) {
-    $page = new rex_be_page('medianeo', 'MediaNeo');
-    $page->setHidden(true);
-    
-    // Registriere die Ajax-Seite
-    $ajaxPage = new rex_be_page('ajax', 'Ajax');
-    $ajaxPage->setPath($this->getPath('pages/ajax.php'));
-    $ajaxPage->setHidden(true);
-    $page->addSubpage($ajaxPage);
-    
-    // Füge die Seite zu REDAXO hinzu
-    $pages = $ep->getSubject();
-    if (isset($pages['mediapool'])) {
-        $pages['mediapool']->addSubpage($page);
-    }
-    
-    return $pages;
-});
