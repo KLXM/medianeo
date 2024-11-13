34ed3479c275887208a8f9bdfc868100407339d3
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-13 20:37:22 +0100

    Update ajax.php

--- pages/ajax.php
+++ pages/ajax.php
@@ -5,8 +5,11 @@ $handler = new rex_medianeo_handler();
 
 try {
     $handler->handleRequest();
-} catch(Exception $e) {
+} catch (Exception $e) {
+    rex_response::setStatus(rex_response::HTTP_INTERNAL_ERROR);
     rex_response::sendJson([
         'error' => $e->getMessage()
     ]);
 }
+
+exit();
