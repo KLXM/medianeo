f938f0b0610d6a8571c6e1781498125add44adcd
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-13 20:46:16 +0100

    Update ajax.php

--- pages/ajax.php
+++ pages/ajax.php
@@ -1,14 +1,22 @@
 <?php
 // Path: redaxo/src/addons/medianeo/pages/ajax.php
 
-$handler = new rex_medianeo_handler();
+// Ensure this is an AJAX request
+if ('XMLHttpRequest' != rex_request::server('HTTP_X_REQUESTED_WITH', 'string', '')) {
+    rex_response::setStatus(rex_response::HTTP_BAD_REQUEST);
+    rex_response::sendJson(['error' => 'Only AJAX requests are allowed']);
+    exit();
+}
 
 try {
+    $handler = new rex_medianeo_handler();
     $handler->handleRequest();
+    
 } catch (Exception $e) {
     rex_response::setStatus(rex_response::HTTP_INTERNAL_ERROR);
     rex_response::sendJson([
-        'error' => $e->getMessage()
+        'error' => $e->getMessage(),
+        'trace' => rex::isDebugMode() ? $e->getTraceAsString() : null
     ]);
 }
 
