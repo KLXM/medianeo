d1bc3ff749c29155466a3f594df65be3ab3b3f90
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-13 20:58:57 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,6 +1,9 @@
 <?php
 // Path: redaxo/src/addons/medianeo/boot.php
 
+// Autoload the API class
+rex_autoload::addDirectory(rex_path::addon('medianeo', 'lib'));
+
 if (rex::isBackend() && rex::getUser()) {
     // Add Sortable.js
     rex_view::addJsFile('https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js');
@@ -13,4 +16,7 @@ if (rex::isBackend() && rex::getUser()) {
     rex_view::setJsProperty('medianeo', [
         'csrf_token' => rex_csrf_token::factory('medianeo')->getValue()
     ]);
+    
+    // Register media manager type
+    rex_medianeo_handler::registerMediaManagerType();
 }
