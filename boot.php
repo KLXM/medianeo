ca5938373eb20178c92dfdd5a39eb35c3bf5427f
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-13 20:15:30 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -2,29 +2,25 @@
 // Path: redaxo/src/addons/medianeo/boot.php
 
 if (rex::isBackend() && rex::getUser()) {
-    // Add assets only when needed
-    if (rex_be_controller::getCurrentPage() == 'mediapool' || 
-        rex_be_controller::getCurrentPage() == 'medianeo') {
-        
-        rex_view::addCssFile($this->getAssetsUrl('css/medianeo.css'));
-        
-        // Add dependencies
-        rex_view::addJsFile('https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js');
-        
-        // Add main script
-        rex_view::addJsFile($this->getAssetsUrl('js/medianeo.js'));
-    }
-}
+    // Add assets
+    rex_view::addJsFile('https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js');
+    rex_view::addCssFile($this->getAssetsUrl('css/medianeo.css'));
+    rex_view::addJsFile($this->getAssetsUrl('js/medianeo.js'));
 
-// Register extension point for custom mediapool buttons
-rex_extension::register('MEDIA_LIST_FUNCTIONS', function (rex_extension_point $ep) {
-    $params = $ep->getParams();
-    $subject = $ep->getSubject();
+    // Add custom initialization script
+    rex_view::addJsFile($this->getAssetsUrl('js/init.js'));
     
-    // Add MediaNeo button if field has medianeo class
-    if (rex_request('opener_input_field', 'string') != '') {
-        $subject .= '<a class="btn btn-xs btn-select medianeo-select" href="#">MediaNeo</a>';
-    }
-    
-    return $subject;
-});
+    // Add custom data for JavaScript
+    rex_view::setJsProperty('medianeo', [
+        'ajax_url' => rex_url::backendController(['page' => 'medianeo/ajax']),
+        'csrf_token' => rex_csrf_token::factory('medianeo')->getValue(),
+        'media_manager_url' => rex_url::backendController(['page' => 'mediapool/medianeo']),
+        'i18n' => [
+            'select_media' => rex_i18n::msg('medianeo_select_media'),
+            'apply' => rex_i18n::msg('medianeo_apply'),
+            'cancel' => rex_i18n::msg('medianeo_cancel'),
+            'no_media_found' => rex_i18n::msg('medianeo_no_media_found'),
+            'loading' => rex_i18n::msg('medianeo_loading')
+        ]
+    ]);
+}
